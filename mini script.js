efb17cbaff4eea4fbb3fb8e5863a67b243f1602f
BigLipBandit <yourfavoreo@gmail.com>
2017-08-07 13:27:20 -0500

    es6?
    i hope that works

---
+++ mini script.js
@@ -0,0 +1,22 @@
+const logger = () => {
+	const message = 'hi';
+	console.log(message);
+};
+
+const looper = () => {
+	const message = 'I love JS!';
+	for (let i = 0; i < 10; i++) {
+		console.log(message);
+	}
+};
+
+const greet = (name) => {
+	const greeting = `Hello ${name}!;
+	console.log(greeting);
+};
+
+const yourName = 'Emmanuel Blackburne';
+
+logger();
+looper();
+greet(yourName);
